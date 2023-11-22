1101fd8aa89861749e74b34f7f19e03672c37bc0
Mue <Mue@mue.com>
2023-11-22 15:30:21 +0300

    ex 00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    while(argv[0][i])
+    {
+        write(1,&argv[0][i],1);
+        i++;
+    }
+    
+}
